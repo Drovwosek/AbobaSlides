d0379db20c9e17cbf1d6e8e0c7f5ad216d582c6a
Barabashhka <pavlik.danilov.2003@mail.ru>
2023-01-09 17:36:18 +0300

    Доработал косяки

--- src/components/objects/Dot.tsx
+++ src/components/objects/Dot.tsx
@@ -26,7 +26,6 @@ function Dot(props: DotProps) {
 
     return (
         <div className={style}
-             draggable='true'
              style={{
                  display: (props.selected) ? ""  : "none", 
              }}
