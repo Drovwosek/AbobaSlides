dfdcd3e233ef1aedfcd80fc28efa9d18dad838cb
Andrey Arsbkv <andrbeckov@gmail.com>
2023-01-11 15:02:49 +0300

    Колдовство какое-то не понял ничего

--- src/model/types/History.ts
+++ src/model/types/History.ts
@@ -1,32 +1,44 @@
 import {ApplicationState} from "./Application";
 
-
+//
 
 const undoStack: Array<ApplicationState> = [];
 const redoStack: Array<ApplicationState> = [];
 
-function addToHistory(application: ApplicationState){
-    undoStack.push({...application})
+function addToHistory(application: ApplicationState) {
+    undoStack.push(application)
+}
+
+function clearRedo(application: ApplicationState) {
+   redoStack.filter(() => false)
 }
 
 function undo(currentState: ApplicationState): ApplicationState {
+    if (!undoStack.length) {
+        return currentState
+    }
+
     const slideState: ApplicationState = undoStack.pop()!;
     redoStack.push(currentState);
 
     return slideState;
 }
 
-function redo(): ApplicationState | undefined{
-    const slidesState: ApplicationState | undefined = redoStack.pop();
-
-    if(slidesState){
-        addToHistory(slidesState);
+function redo(): ApplicationState | undefined {
+    if (!redoStack.length) {
+        return undefined
     }
+
+    const slidesState: ApplicationState = redoStack.pop()!;
+
+        addToHistory(slidesState);
+
     return slidesState;
 }
 
 export{
     addToHistory,
     undo,
-    redo
+    redo,
+    clearRedo,
 }
